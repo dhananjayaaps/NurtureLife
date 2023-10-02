8fe9d4f3e02c4052c042a72438a84e2e11d778e3
dhananjayaaps <dhananjayaaps@gmail.com>
2023-10-02 10:13:06 +0530

    logout added

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -56,4 +56,10 @@ class AuthController extends Controller
             'model' => $user
         ]);
     }
+
+    public function logout(Request $request, Response $response)
+    {
+        Application::$app->logout();
+        $response->redirect('/');
+    }
 }
--- public/index.php
+++ public/index.php
@@ -30,4 +30,6 @@ $app->router->post('/login', [AuthController::class, 'login']);
 $app->router->post('/register', [AuthController::class, 'register']);
 $app->router->get('/register', [AuthController::class, 'register']);
 
+$app->router->get('/logout', [AuthController::class, 'logout']);
+
 $app->run();
