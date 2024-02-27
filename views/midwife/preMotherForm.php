fb7f28af4c5ff65b168b523bf98827d867475dce
dhananjayaaps <dhananjayaaps@gmail.com>
2024-02-27 14:35:51 +0530

    pre mother form edited

--- views/midwife/preMotherForm.php
+++ views/midwife/preMotherForm.php
@@ -57,7 +57,8 @@ $this->title = 'Mothers';
                     $maritalStatusField = new Dropdown($model, 'MaritalStatus', 'Marital Status');
                     $maritalStatusField->setOptions([
                         'married' => 'Married',
-                        'unmarried' => 'Unmarried',
+                        'single' => 'Single Mother',
+                        'divorce' => 'Divorced',
                     ]);
                     echo $maritalStatusField;
                     ?>
@@ -65,12 +66,13 @@ $this->title = 'Mothers';
                     <?php
                     $bloodGroupField = new DropDown($model, 'BloodGroup', 'Blood Group');
                     $bloodGroupField->setOptions([
-                        'A+' => 'A+',
-                        'A-' => 'A-',
-                        'B+' => 'B+',
-                        'B-' => 'B-',
-                        'O+' => 'O+',
-                        'O-' => 'O-',
+                        'A+' => 'A positive (A+)',
+                        'A-' => 'A negative (A-)',
+                        'B+' => 'B positive (B+)',
+                        'B-' => 'B negative (B-)',
+                        'AB+' => 'AB positive (AB+)',
+                        'O+' => 'O positive (O+)',
+                        'O-' => 'O negative (O-)',
                     ]);
                     echo $bloodGroupField;
                     ?>
@@ -82,18 +84,71 @@ $this->title = 'Mothers';
             </div>
 
             <div class="form-column">
-                <?php echo $form->field($model, 'Consanguinity', 'Consanguinity')?>
-                <?php echo $form->field($model, 'history_subfertility', 'History Subfertility')?>
-                <?php echo $form->field($model, 'Hypertension', 'Hypertension')?>
-                <?php echo $form->field($model, 'diabetes_mellitus', 'Diabetes Mellitus')?>
-                <?php
-                $rubellaImmunizationField = new Dropdown($model, 'rubella_immunization', 'Rubella Immunization');
-                $rubellaImmunizationField->setOptions([
-                    1 => 'Yes',
-                    2 => 'No',
-                ]);
-                echo $rubellaImmunizationField;
-                ?>
+<!--                --><?php //echo $form->field($model, 'Consanguinity', 'Consanguinity')?>
+<!--                --><?php //echo $form->field($model, 'history_subfertility', 'History Subfertility')?>
+<!--                --><?php //echo $form->field($model, 'Hypertension', 'Hypertension')?>
+<!--                --><?php //echo $form->field($model, 'diabetes_mellitus', 'Diabetes Mellitus')?>
+<!--                --><?php
+//                $rubellaImmunizationField = new Dropdown($model, 'rubella_immunization', 'Rubella Immunization');
+//                $rubellaImmunizationField->setOptions([
+//                    1 => 'Yes',
+//                    2 => 'No',
+//                ]);
+//                echo $rubellaImmunizationField;
+//                ?>
+
+                <style>
+                    .break-line {
+                        border-top: 1px solid #FFF;
+                        margin-top: 12px;
+                        margin-bottom: 10px;
+                    }
+                </style>
+
+                <label>
+                    Consanguinity:
+                    <div class="break-line"></div>
+                    <input type="radio" name="Consanguinity" value="Yes"> Yes
+                    <input type="radio" name="Consanguinity" value="No"> No
+                </label>
+
+                <div class="break-line"></div>
+
+                <label>
+                    History Subfertility:
+                    <div class="break-line"></div>
+                    <input type="radio" name="history_subfertility" value="Yes"> Yes
+                    <input type="radio" name="history_subfertility" value="No"> No
+                </label>
+
+                <div class="break-line"></div>
+
+                <label>
+                    Hypertension:
+                    <div class="break-line"></div>
+                    <input type="radio" name="Hypertension" value="Yes"> Yes
+                    <input type="radio" name="Hypertension" value="No"> No
+                </label>
+
+                <div class="break-line"></div>
+
+                <label>
+                    Diabetes Mellitus:
+                    <div class="break-line"></div>
+                    <input type="radio" name="diabetes_mellitus" value="Yes"> Yes
+                    <input type="radio" name="diabetes_mellitus" value="No"> No
+                </label>
+
+                <div class="break-line"></div>
+
+                <label>
+                    Rubella Immunization:
+                    <div class="break-line"></div>
+                    <input type="radio" name="rubella_immunization" value="Yes"> Yes
+                    <input type="radio" name="rubella_immunization" value="No"> No
+                </label>
+
+                <div class="break-line"></div>
 
                 <?php echo $form->field($model, 'emergencyNumber', 'Emergency Number')?>
             </div>
