8693186e7d48c240427ecbbac19ea4014e9a7040
cheya01 <2021cs206@stu.ucsc.cmb.ac.lk>
2024-04-19 20:07:21 +0530

    CSS corrections

--- views/layouts/volunteer.php
+++ views/layouts/volunteer.php
@@ -3,12 +3,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?=$this->title?></title>
-<link rel="icon" type="image/x-icon" href="./assets/images/icons/favicon.png">
-<link rel="stylesheet" href="./assets/styles/styles.css">
-<link rel="stylesheet" href="./assets/styles/volunteer.css">
+    <link rel="icon" type="image/x-icon" href="./assets/images/icons/favicon.png">
+    <link rel="stylesheet" href="./assets/styles/styles.css">
+    <link rel="stylesheet" href="./assets/styles/volunteer.css">
 </head>
 <body>
 
