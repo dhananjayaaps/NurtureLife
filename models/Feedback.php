d0fae2003dbc0b70350ce6ee67a9b6c4df6dfebd
cheya01 <2021cs206@stu.ucsc.cmb.ac.lk>
2024-04-27 12:30:18 +0530

    edited feedback model

--- models/Feedback.php
+++ models/Feedback.php
@@ -50,61 +50,23 @@ class Feedback extends DbModel
         return ($data);
     }
 
-//    public function getPosts(): string
-//    {
-//        if(Application::$app->user->getRoleName() == 'Volunteer'){
-//            $joins = [
-//                ['model' => User::class, 'condition' => 'post.user_id = users.id'],
-//            ];
-//            $postData = (new Post())->findAllWithJoins(self::class, $joins, ['postal_code' => Application::$app->user->getZip()]);
-//
-//        }else{
-//            $postData = (new Post())->findAll(self::class, ['user_id' => Application::$app->user->getId()]);
-//        }
-//        $data = [];
-//
-//        foreach ($postData as $post) {
-//            $data[] = [
-//                'id' => $post->id,
-//                'user_id' => $post->user_id,
-//                'description' => $post->description,
-//                'created_at' => $post->created_at,
-//                'updated_at' => $post->updated_at,
-//                'status' => $post->status
-//            ];
-//        }
-//        return json_encode($data);
-//    }
+    public function getFeedbackById($FeedbackId): string
+    {
+        $this->id = $FeedbackId;
+        $feedbackData = (new Post())->findOne(self::class, ['id' => $FeedbackId]);
 
-//    public function getPostById($PostId): string
-//    {
-//        $this->id = $PostId;
-//        $postData = (new Post())->findOne(self::class, ['id' => $PostId]);
-//
-//        $data = [
-//            'id' => $postData->id,
-//            'user_id' => $postData->user_id,
-//            'description' => $postData->description,
-//            'created_at' => $postData->created_at,
-//            'updated_at' => $postData->updated_at,
-//            'status' => $postData->status
-//        ];
-//        return json_encode($data);
-//    }
-
-//    public function getAPost($PostId)
-//    {
-//        return (new Post())->findOne(self::class, ['id' => $PostId]);
-//    }
+        $data = [
+            'id' => $FeedbackId->id,
+            'user_id' => $FeedbackId->email,
+            'description' => $FeedbackId->feedback,
+            'created_at' => $FeedbackId->created_at,
+        ];
+        return json_encode($data);
+    }
 
     public function attributes(): array
     {
         return ['email', 'feedback', 'created_at'];
     }
-
-//    public function update() : bool
-//    {
-//        return parent::update();
-//    }
 
 }
